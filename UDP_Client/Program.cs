3c8dfd9ce154df3949a8503bab9d3d5ac171ef71
Ищенко Вячеслав <ishchenkoviacheslav@live.com>
2019-04-05 18:05:23 +0300

    must add 11001 port to client

--- UDP_Client/Program.cs
+++ UDP_Client/Program.cs
@@ -13,7 +13,7 @@ namespace UdpClientApp
     public class UDPListener
     {
         private static IConfigurationRoot configuration;
-        private static int Client_listenPort = 0;
+        //private static int Client_listenPort = 0;
         private static int Server_listenPort = 0;
         private static string server_ip = string.Empty;
         ///ping
@@ -29,37 +29,37 @@ namespace UdpClientApp
             //work slow for ping!
             Console.WriteLine("Waiting ...");
 
-            Client_listenPort = int.Parse(configuration["client_listenPort"]);
+            //Client_listenPort = int.Parse(configuration["client_listenPort"]);
             Server_listenPort = int.Parse(configuration["server_listenPort"]);
             server_ip = configuration.GetSection("serverip").Value;
             pauseBetweenPing = int.Parse(configuration["pausebetweenping"]);
             pauseBetweenSendData = int.Parse(configuration["pauseBetweenSendData"]);
-            if (Client_listenPort == 0 || Server_listenPort == 0 || string.IsNullOrEmpty(server_ip) || pauseBetweenPing < 10 || pauseBetweenSendData < 10)
+            if (/*Client_listenPort == 0 ||*/ Server_listenPort == 0 || string.IsNullOrEmpty(server_ip) || pauseBetweenPing < 10 || pauseBetweenSendData < 10)
                 throw new Exception("configuration data is wrong");
             Console.WriteLine("*********Client*******");
-            UdpClient listener = new UdpClient(Client_listenPort);
-            UdpClient sender = new UdpClient();
+            UdpClient listener = new UdpClient(11001);
+            //UdpClient sender = new UdpClient();
 
             IPEndPoint groupEP = null; // new IPEndPoint(IPAddress.Any, listenPort);
-            Task.Run(() =>
+            Task.Run(async () =>
             {
                 try
                 {
                     while (true)
                     {
-                        //listen on 11001
-                        byte[] bytes = listener.Receive(ref groupEP);
-
-                        if(bytes.SequenceEqual(ping))
+                        UdpReceiveResult result = await listener.ReceiveAsync();
+                        groupEP = result.RemoteEndPoint;
+                        byte[] bytes = result.Buffer;
+                        if (bytes.SequenceEqual(ping))
                         {
                             inputTime.Add(DateTime.Now);
                         }
-                        ////else
-                        ////{
-                        ////    //if ping than don't need wait when cw is finished
-                        ////    Console.WriteLine($"Received from {groupEP} :");
-                        ////    Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
-                        ////}
+                        else
+                        {
+                            //if ping than don't need wait when cw is finished
+                            Console.WriteLine($"Received from {groupEP} :");
+                            Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+                        }
                     }
                 }
                 catch (SocketException e)
@@ -72,19 +72,19 @@ namespace UdpClientApp
                 }
             });
             //ping proccess
-            Task.Run(() => 
+            Task.Run(() =>
             {
                 while (true)
                 {
                     for (int i = 0; i < 5; i++)
                     {
                         outputTime.Add(DateTime.Now);
-                        sender.Send(ping, ping.Length, server_ip, Server_listenPort);
+                        listener.SendAsync(ping, ping.Length, server_ip, Server_listenPort);
                     }
                     Thread.Sleep(pauseBetweenPing);
                     List<TimeSpan> timeSpan = new List<TimeSpan>();
                     //if 5 == 5 ...
-                    if(outputTime.Count == inputTime.Count)
+                    if (outputTime.Count == inputTime.Count)
                     {
                         for (int i = 0; i < outputTime.Count; i++)
                         {
@@ -105,24 +105,24 @@ namespace UdpClientApp
                 }
             });
             //type some message
-            //Task.Run(() =>
-            //{
-            while (true)
+            Task.Run(() =>
             {
-                //Console.Write("Enter message: ");
-                //string message = Console.ReadLine();
-                //Thread.Sleep(pauseBetweenSendData);
-                string message = "test";
-                if (message == "stop")
+                while (true)
                 {
-                    break;
+                    //Console.Write("Enter message: ");
+                    //string message = Console.ReadLine();
+                    Thread.Sleep(pauseBetweenSendData);
+                    string message = "test";
+                    if (message == "stop")
+                    {
+                        break;
+                    }
+                    byte[] myString = Encoding.ASCII.GetBytes(message);
+                    //umc 46.133.172.211
+                    //ukrtelecom 92.112.59.89
+                    listener.SendAsync(myString, myString.Length, server_ip, Server_listenPort);
                 }
-                byte[] myString = Encoding.ASCII.GetBytes(message);
-                //umc 46.133.172.211
-                //ukrtelecom 92.112.59.89
-                sender.Send(myString, myString.Length, server_ip, Server_listenPort);
-            }
-            //});
+            });
             Console.ReadLine();
         }
 
@@ -134,6 +134,41 @@ namespace UdpClientApp
 
             configuration = builder.Build();
             StartListener();
+
+
+            //IPAddress serverIP = IPAddress.Parse("127.0.0.1");     // Server IP
+            //int port = 27005;                                           // Server port
+            //IPEndPoint ipEndPoint = new IPEndPoint(serverIP, port);
+            //string serverResponse = string.Empty;       // The variable which we will use to store the server response
+
+            //using (UdpClient client = new UdpClient())
+            //{
+            //    byte[] data = Encoding.UTF8.GetBytes("I am client");      // Convert our message to a byte array
+            //    client.Send(data, data.Length, ipEndPoint);      // Send the date to the server
+
+            //    serverResponse = Encoding.UTF8.GetString(client.Receive(ref ipEndPoint));    // Retrieve the response from server as byte array and convert it to string
+            //}
+            //Console.WriteLine(serverResponse);
+
+            //work good
+            //Task.Run(async () =>
+            //{
+            //    Console.WriteLine("Client");
+            //    IPAddress serverIP = IPAddress.Parse("127.0.0.1");     // Server IP
+            //    int port = 27005;                                           // Server port
+            //    IPEndPoint ipEndPoint = new IPEndPoint(serverIP, port);
+            //    string serverResponse = string.Empty;       // The variable which we will use to store the server response
+
+            //    using (UdpClient client = new UdpClient())
+            //    {
+            //        byte[] data = Encoding.UTF8.GetBytes("I am client");      // Convert our message to a byte array
+            //        await client.SendAsync(data, data.Length, ipEndPoint);      // Send the date to the server
+            //        UdpReceiveResult result = await client.ReceiveAsync();
+            //        serverResponse = Encoding.UTF8.GetString(result.Buffer);    // Retrieve the response from server as byte array and convert it to string
+            //    }
+            //    Console.WriteLine(serverResponse);
+            //});
+            //Console.ReadLine();
         }
     }
 
