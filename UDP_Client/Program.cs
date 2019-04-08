e13889cc8f034e6619e09ec73f62496cda15c212
Ищенко Вячеслав <ishchenkoviacheslav@live.com>
2019-04-08 12:38:08 +0300

    it work!!! But only if server on home pc, and client on mobile(server on mobile inet not work)

--- UDP_Client/Program.cs
+++ UDP_Client/Program.cs
@@ -13,7 +13,7 @@ namespace UdpClientApp
     public class UDPListener
     {
         private static IConfigurationRoot configuration;
-        //private static int Client_listenPort = 0;
+        private static int Client_listenPort = 0;
         private static int Server_listenPort = 0;
         private static string server_ip = string.Empty;
         ///ping
@@ -29,15 +29,15 @@ namespace UdpClientApp
             //work slow for ping!
             Console.WriteLine("Waiting ...");
 
-            //Client_listenPort = int.Parse(configuration["client_listenPort"]);
+            Client_listenPort = int.Parse(configuration["client_listenPort"]);
             Server_listenPort = int.Parse(configuration["server_listenPort"]);
             server_ip = configuration.GetSection("serverip").Value;
             pauseBetweenPing = int.Parse(configuration["pausebetweenping"]);
             pauseBetweenSendData = int.Parse(configuration["pauseBetweenSendData"]);
-            if (/*Client_listenPort == 0 ||*/ Server_listenPort == 0 || string.IsNullOrEmpty(server_ip) || pauseBetweenPing < 10 || pauseBetweenSendData < 10)
+            if (Client_listenPort == 0 || Server_listenPort == 0 || string.IsNullOrEmpty(server_ip) || pauseBetweenPing < 10 || pauseBetweenSendData < 10)
                 throw new Exception("configuration data is wrong");
             Console.WriteLine("*********Client*******");
-            UdpClient listener = new UdpClient(11001);
+            UdpClient listener = new UdpClient(Client_listenPort);
             //UdpClient sender = new UdpClient();
 
             IPEndPoint groupEP = null; // new IPEndPoint(IPAddress.Any, listenPort);
